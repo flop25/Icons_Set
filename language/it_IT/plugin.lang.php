6cf146360f04e6caaaf1dda43bf4612b1b14f2ca
riopwg <riopwg@piwigo.org>
2011-04-18 11:14:54 +0000

    language IT for Icons_Set
    git-svn-id: http://piwigo.org/svn/extensions/Icons_Set@10470 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,19 @@
+<?php
+$lang['iconset_error_update'] = 'Un errore è apparso durante l\'aggiornamento di questo(i) tema(i) (riprovare o chiedere aiuto su it.piwigo.org/forum : ';
+$lang['iconset_update_ok'] = 'La configurazioe di questo(i) tema(i) è stata aggiornata : ';
+$lang['iconset_info_new_theme'] = 'Questo(i) tema(i) è/sono stato(i) aggiunto(i) : ';
+$lang['iconset_info_new_icon'] = 'Questo(i) set è/sono stato(i) aggiunto(i) : ';
+$lang['iconset_info_deleted_theme'] = 'Questo(i) tema(i) è/sono stato(i) cancellato(i) : ';
+$lang['iconset_info_deleted_icon'] = 'Questo(i) set è/sono stato(i) cancellato(i) : ';
+
+$lang['iconset_configpage'] = 'Pagina di configurazione d\'Icons Set';
+
+$lang['iconset_All_Themes'] = 'Attribuire, di seguito, i set d\'icone ai temi';
+$lang['iconset_nothing'] = 'icone d\'origine';
+
+$lang['iconset_send'] = 'Registrare les modifiche effettuate';
+$lang['iconset_All_Icons'] = 'Liste dei set d\'icone disponibili';
+$lang['iconset_seeall'] = 'Vederle tutte';
+
+
+?>
