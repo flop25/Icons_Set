12a59e093e6957222c8ebfc1b83b436be44be630
ddtddt <ddtddt@piwigo.org>
2011-04-18 12:21:36 +0000

    [extensions] - Icons_Set -  add lv_LV (Latvian) thanks to Aivars Baldone
    git-svn-id: http://piwigo.org/svn/extensions/Icons_Set@10478 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,19 @@
+<?php
+$lang['iconset_error_update'] = 'šīs(o) tēmas(u) jaunināšanas gaitā radusies kļūme (lūdzu mēģiniet vēlreiz vai meklējiet risinājumu forumā piwigo.org/forum) : ';
+$lang['iconset_update_ok'] = 'šīs(o) tēmas(u) konfigurācija atjaunināta(s) : ';
+$lang['iconset_info_new_theme'] = 'Šī(s) tēma(s) izdzēsta(s) : ';
+$lang['iconset_info_new_icon'] = 'Šis(o) ikonu komplekts pievienots : ';
+$lang['iconset_info_deleted_theme'] = 'Šī(s) tēma(s) izdzēsta(s) : ';
+$lang['iconset_info_deleted_icon'] = 'Šis(o) ikonu komplekts izdzēsts : ';
+
+$lang['iconset_configpage'] = 'Ikonu komplekta Konfigurācijas Lapa';
+
+$lang['iconset_All_Themes'] = 'Izvēlaties ikonu komplektu: visām zemāk esošajām tēmām :';
+$lang['iconset_nothing'] = "oriģinālās ikonas";
+
+$lang['iconset_send'] = "Saglabāt konfigurācijas modifikācijas";
+$lang['iconset_All_Icons'] = "Visi ikonu komplekti";
+$lang['iconset_seeall'] = "Skatīt visu";
+
+
+?>
