62d735997de3219e51857664604154b7dc41065f
riopwg <riopwg@piwigo.org>
2011-04-18 11:14:03 +0000

    Update language FR
    git-svn-id: http://piwigo.org/svn/extensions/Icons_Set@10469 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/fr_FR/plugin.lang.php
+++ language/fr_FR/plugin.lang.php
@@ -1,10 +1,10 @@
 <?php
 $lang['iconset_error_update'] = 'Une erreur a eut lieu lors de la mise à jour de ce(s) thème(s) (réessayez ou demandez de l\'aide sur fr.piwigo.org/forum : ';
-$lang['iconset_update_ok'] = 'La config de ce(s) thèmes a été mise à jour : ';
-$lang['iconset_info_new_theme'] = 'Ce(s) thème(s) a/ont été ajoutés : ';
-$lang['iconset_info_new_icon'] = 'Ce(s) set(s) a/ont été ajoutés : ';
-$lang['iconset_info_deleted_theme'] = 'Ce(s) thème(s) a/ont été supprimés : ';
-$lang['iconset_info_deleted_icon'] = 'Ce(s) set(s) a/ont été supprimés : ';
+$lang['iconset_update_ok'] = 'La config de ce(s) thèmes a/ont été mise à jour : ';
+$lang['iconset_info_new_theme'] = 'Ce(s) thème(s) a/ont été ajouté(s) : ';
+$lang['iconset_info_new_icon'] = 'Ce(s) set(s) a/ont été ajouté(s) : ';
+$lang['iconset_info_deleted_theme'] = 'Ce(s) thème(s) a/ont été supprimé(s) : ';
+$lang['iconset_info_deleted_icon'] = 'Ce(s) set(s) a/ont été supprimé(s) : ';
 
 $lang['iconset_configpage'] = 'Page Config d\'Icons Set';
 
