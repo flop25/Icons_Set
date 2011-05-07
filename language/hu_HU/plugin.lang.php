0a5114659ffc1a4b4f533ddd127002ee393e0a41
samli <samli@piwigo.org>
2011-05-07 12:30:28 +0000

    [extensions] [Icons_Set] add HU language
    git-svn-id: http://piwigo.org/svn/extensions/Icons_Set@10806 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/hu_HU/plugin.lang.php
@@ -0,0 +1,18 @@
+<?php
+$lang['iconset_error_update'] = 'Hiba történt a téma/témák frissítésekor (próbáld újra, illetve jelezd a hu.piwigo.org/forum oldalon) : ';
+$lang['iconset_update_ok'] = 'A következő téma/témák frissítése sikeres : ';
+$lang['iconset_info_new_theme'] = 'A következő témát/témákat adtuk hozzá : ';
+$lang['iconset_info_new_icon'] = 'A következő ikonkészletet/ikonkészleteket adtuk hozzá : ';
+$lang['iconset_info_deleted_theme'] = 'A következő témát/témákat töröltük : ';
+$lang['iconset_info_deleted_icon'] = 'A következő ikonkészletet/ikonkészleteket töröltük : ';
+
+$lang['iconset_configpage'] = 'Icons Set konfigurációs oldal';
+
+$lang['iconset_All_Themes'] = 'Az összes téma alatt: ikonkészlet kiválasztása';
+$lang['iconset_nothing'] = "eredeti ikonok";
+
+$lang['iconset_send'] = "Módosítások mentése";
+$lang['iconset_All_Icons'] = "Összes ikonkészlet";
+$lang['iconset_seeall'] = "Összes megjelenítése";
+
+?>
