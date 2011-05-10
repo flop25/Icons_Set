4985046c98c8e6e118a6222660503fc7ff8316c9
flop25 <flop25@piwigo.org>
2011-05-10 15:25:07 +0000

    adding default/iconset.conf.php
    git-svn-id: http://piwigo.org/svn/extensions/Icons_Set@10836 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ icons/default/iconset.conf.php
@@ -0,0 +1,10 @@
+<?php
+$iconsetconf = array(
+  'name' => 'Default',
+  'id' => 'default',
+  'icon_file' => ICONSET_PATH.'icons/default/sprite.png',
+  'css_file' => ICONSET_PATH.'icons/default/default.css',
+  'css_file_admin' => ICONSET_PATH.'icons/default/icon_admin.css',
+);
+
+?>
