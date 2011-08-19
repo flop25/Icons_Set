57933f0ee38b3f6483872aea57ca68a66ddf4433
ddtddt <ddtddt@piwigo.org>
2011-08-19 10:17:25 +0000

    [extensions] - Icons_Set - add pl_PL - Polski - Polish - Thanks to kuba
    git-svn-id: http://piwigo.org/svn/extensions/Icons_Set@11973 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pl_PL/plugin.lang.php
@@ -0,0 +1,17 @@
+<?php
+$lang['iconset_error_update'] = 'błąd przy aktualizowaniu tematu (spróbuj ponownie lub zgłoś błąd na piwigo.org/forum) : ';
+$lang['iconset_update_ok'] = 'konfiguracja tematu zaktualizowana: ';
+$lang['iconset_info_new_theme'] = 'Temat został dodany: ';
+$lang['iconset_info_new_icon'] = 'Zestaw ikon został dodany: ';
+$lang['iconset_info_deleted_theme'] = 'Temat został usunięty: ';
+$lang['iconset_info_deleted_icon'] = 'Zestaw ikon został usunięty:: ';
+
+$lang['iconset_configpage'] = 'Icons Set Config Page';
+
+$lang['iconset_All_Themes'] = 'All the themes are below : select an icons set';
+$lang['iconset_nothing'] = "oryginalne ikony";
+
+$lang['iconset_send'] = "Zapisz zmiany konfiguracji";
+$lang['iconset_All_Icons'] = "Ustawiono wszystkie ikony";
+$lang['iconset_seeall'] = "Zobacz wszystkie"; 
+?>
