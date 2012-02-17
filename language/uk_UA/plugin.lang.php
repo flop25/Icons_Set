986c9cf55ef9acc338fb67a1310a09fa18f0439c
flop25 <flop25@piwigo.org>
2012-02-17 09:03:21 +0000

    adding uk_UA thx to animan (pixobox.org)
    git-svn-id: http://piwigo.org/svn/extensions/Icons_Set@13206 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,18 @@
+<?php
+$lang['iconset_error_update'] = 'сталася помилка під час оновлення цих/цієї тем(и) (будь ласка, повторіть або опублікуйте на piwigo.org/forum) : ';
+$lang['iconset_update_ok'] = 'конфігурації цих/цієї тем(и) був оновлений : ';
+$lang['iconset_info_new_theme'] = 'Цей набір іконок для тем(и) були додані : ';
+$lang['iconset_info_new_icon'] = 'Цей набір іконок були додані : ';
+$lang['iconset_info_deleted_theme'] = 'Цей набір іконок для тем(и) був видалений : ';
+$lang['iconset_info_deleted_icon'] = 'Цей набір іконок був видалений : ';
+
+$lang['iconset_configpage'] = 'Сторінка налаштування набору іконок';
+
+$lang['iconset_All_Themes'] = 'Всі теми наведені нижче: вибрати набір іконок';
+$lang['iconset_nothing'] = "оригінальні іконки";
+
+$lang['iconset_send'] = "Зберегти зміни конфігурації";
+$lang['iconset_All_Icons'] = "Всі встановлені іконки";
+$lang['iconset_seeall'] = "Переглянути всі";
+
+?>
