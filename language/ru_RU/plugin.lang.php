aee293bb30fcfa9cf1b944f4f116a0b22d713e9a
TranslationTeam <TranslationTeam@piwigo.org>
2012-04-11 10:49:32 +0000

    [Icons Set] Add ru_RU, thanks to : nadusha
    git-svn-id: http://piwigo.org/svn/extensions/Icons_Set@14091 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['iconset_All_Icons'] = 'Все наборы иконок';
+$lang['iconset_All_Themes'] = 'Все темы приведены ниже: выбрать набор иконок';
+$lang['iconset_configpage'] = 'Страница конфигурации Наборов иконок';
+$lang['iconset_error_update'] = 'Произошла ошибка во время обновления этой/этих тем (пожалуйста, повторите или разместить на piwigo.org или в форуме):';
+$lang['iconset_info_deleted_icon'] = 'Эта/эти набор(ы) были добавлены:';
+$lang['iconset_info_deleted_theme'] = 'Эта/эти набор(ы) были удалена(ы):';
+$lang['iconset_info_new_icon'] = 'Эта/эти тема(ы) были добавлены:';
+$lang['iconset_info_new_theme'] = 'Эта/эти тема(ы) были удалена(ы):';
+$lang['iconset_nothing'] = 'оригинальные иконки';
+$lang['iconset_seeall'] = 'Смотреть все';
+$lang['iconset_send'] = 'Сохранить изменения конфигурации';
+$lang['iconset_update_ok'] = 'конфигурации этой/этих тем была обновлена:';
+?>
