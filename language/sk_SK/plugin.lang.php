794ff5a1a7505d2048e5221d983a8b22c8bf5bac
TranslationTeam <TranslationTeam@piwigo.org>
2012-04-12 09:06:37 +0000

    [Icons Set] Add sk_SK, thanks to : dodo
    git-svn-id: http://piwigo.org/svn/extensions/Icons_Set@14113 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['iconset_All_Icons'] = 'Všetky súbory ikon';
+$lang['iconset_All_Themes'] = 'Všetky témy sú nižšie: vybrať súbor ikon';
+$lang['iconset_configpage'] = 'Konfiguračná stránka súboru ikon';
+$lang['iconset_error_update'] = 'vyskytla sa chyba pri aktualizácii tejto/týchto tém(y) (prosím zopakujte, alebo pošlite na  piwigo.org/forum):';
+$lang['iconset_info_deleted_icon'] = 'Tento/Tieto súbor(y) ikon boli vymazané:';
+$lang['iconset_info_deleted_theme'] = 'Táto/Tieto téma(y) boli vymazané:';
+$lang['iconset_info_new_icon'] = 'Tento/Tieto súbor(y) ikon boli pridané:';
+$lang['iconset_info_new_theme'] = 'Táto/Tieto téma(y) boli pridané:';
+$lang['iconset_nothing'] = 'originálne ikony';
+$lang['iconset_seeall'] = 'Pozrieť všetko';
+$lang['iconset_send'] = 'Uložiť modifikácie konfigurácie';
+$lang['iconset_update_ok'] = 'konfigurácia tejto/týchto tém(y) boli aktualizované:';
+?>
