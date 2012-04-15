12e7342a16b632de39df04f4dc2b3167a271bb40
flop25 <flop25@piwigo.org>
2012-04-15 16:14:22 +0000

    features : -grum-dark-II added
    bugs :
    -gbo, sylvia sprite updated
    -sets with a sprite which does not contain new icons of 2.4, use tango ones
    
    git-svn-id: http://piwigo.org/svn/extensions/Icons_Set@14167 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ icons/grum-dark-II/iconset.conf.php
@@ -0,0 +1,10 @@
+<?php
+$iconsetconf = array(
+  'name' => 'grum dark II',
+  'id' => 'grum_dark_II',
+  'icon_file' => ICONSET_PATH.'icons/grum-dark-II/icons_sprite.png',
+  'css_file' => ICONSET_PATH.'icons/grum-dark-II/icon.css',
+  'css_file_admin' => ICONSET_PATH.'icons/grum-dark-II/icon_admin.css',
+);
+
+?>
