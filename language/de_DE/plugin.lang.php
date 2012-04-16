4e11f34df79e6bb0eb6ec5828844c2c4e10c617f
TranslationTeam <TranslationTeam@piwigo.org>
2012-04-16 11:38:31 +0000

    [Icons Set] Add de_DE, thanks to : Polly
    git-svn-id: http://piwigo.org/svn/extensions/Icons_Set@14183 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['iconset_All_Icons'] = 'Das ganze Icon Set';
+$lang['iconset_All_Themes'] = 'Alle Themes sind unten: wähle ein Icon Set';
+$lang['iconset_configpage'] = 'Konfiguration Icon Set';
+$lang['iconset_error_update'] = 'ein Fehler ist während der Aktualisierung des/der Theme(s) aufgetreten (Bitte nochmals versuchen oder einen Beitrag im Forum schreiben):';
+$lang['iconset_info_deleted_icon'] = 'Diese(s) Icon Set(s) wurde gelöscht:';
+$lang['iconset_info_deleted_theme'] = 'Diese(s) Theme(s) wurde gelöscht:';
+$lang['iconset_info_new_icon'] = 'Diese(s) Icon(s) wurden hinzugefügt';
+$lang['iconset_info_new_theme'] = 'Diese(s) Theme(s) wurde gelöscht:';
+$lang['iconset_nothing'] = 'original Icons';
+$lang['iconset_seeall'] = 'Alle anzeigen';
+$lang['iconset_send'] = 'Speichere die Änderungen der Konfiguration';
+$lang['iconset_update_ok'] = 'die Konfiguration des/der Theme(s) wurde aktualisiert:';
+?>
