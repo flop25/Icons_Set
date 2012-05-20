b56b0d5ba71aee6decde11103e63073aa429b02b
TranslationTeam <TranslationTeam@piwigo.org>
2012-05-20 21:29:38 +0000

    [icons_set] Add nl_NL, thanks to : pauldaytona
    git-svn-id: http://piwigo.org/svn/extensions/Icons_Set@15160 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['iconset_All_Icons'] = 'Alle icons set';
+$lang['iconset_All_Themes'] = 'Alle themes staan hieronder: kies een Icons set';
+$lang['iconset_configpage'] = 'Icons Set configuratie pagina';
+$lang['iconset_error_update'] = 'een fout is opgetreden gedurende een update van dit/deze theme(s)(Probeer opnieuw of post in het piwigo.org/forum):';
+$lang['iconset_info_deleted_icon'] = 'Deze icons set(s)zijn verwijded:';
+$lang['iconset_info_deleted_theme'] = 'Deze theme(s)zijn verwijded:';
+$lang['iconset_info_new_icon'] = 'Deze icons set(s)zijn toegevoegd:';
+$lang['iconset_info_new_theme'] = 'Deze theme(s)zijn toegevoegd:';
+$lang['iconset_nothing'] = 'Orginele icons';
+$lang['iconset_seeall'] = 'bekijk alles';
+$lang['iconset_send'] = 'Bewaar wijzigingen in de configuratie';
+$lang['iconset_update_ok'] = 'De configuratie van dit/deze theme(s) zijn geupdate:';
+?>
