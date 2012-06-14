fced798d715023cf825ecfa6690c9bc4db1fcad1
TranslationTeam <TranslationTeam@piwigo.org>
2012-06-14 22:32:39 +0000

    [icons_set] Update sv_SE, thanks to : styxsailor
    git-svn-id: http://piwigo.org/svn/extensions/Icons_Set@15712 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['iconset_All_Icons'] = 'Alla ikoner är angivna';
+$lang['iconset_All_Themes'] = 'Alla teman är listade nedan: välj en ikongrupp';
+$lang['iconset_configpage'] = 'Inställningar för ikongrupp';
+$lang['iconset_error_update'] = 'ett fel inträffade vid uppdatering av detta/dessa tema/n (försök igen eller meddela piwigo.org/forum)';
+$lang['iconset_info_deleted_icon'] = 'Detta/dessa ikongrupp/er har tagits bort:';
+$lang['iconset_info_deleted_theme'] = 'Detta/dessa tema/n har tagits bort:';
+$lang['iconset_info_new_icon'] = 'Detta/dessa ikongrupp/er har lagts till:';
+$lang['iconset_info_new_theme'] = 'Detta/dessa tema/n har lagts till:';
+$lang['iconset_nothing'] = 'originalikoner';
+$lang['iconset_seeall'] = 'Visa alla';
+$lang['iconset_send'] = 'Spara ändringar av inställningar';
+$lang['iconset_update_ok'] = 'inställningarna av detta/dessa tema/n har uppdaterats';
+?>
