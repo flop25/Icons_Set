3da6f8c3ca338463710011e95fef95789c5ee520
TranslationTeam <TranslationTeam@piwigo.org>
2012-06-18 05:07:03 +0000

    [icons_set] Update tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/Icons_Set@15811 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['iconset_All_Icons'] = 'Tüm ikon setleri';
+$lang['iconset_All_Themes'] = 'Aşağıdaki tüm temalar: bir ikon seti seçin';
+$lang['iconset_configpage'] = 'Ikon Seti Konfigürasyon Sayfası';
+$lang['iconset_error_update'] = 'bu tema/temaların güncellenmesi sırasında bir hata oluştu (lütfen tekrar deneyin veya piwigo.org/forum \'a yazın)';
+$lang['iconset_info_deleted_icon'] = 'Bu ikon seti/setleri silinmiştir:';
+$lang['iconset_info_deleted_theme'] = 'Bu tema/temalar silinmiştir.';
+$lang['iconset_info_new_icon'] = 'Bu ikon seti/setleri eklenmiştir:';
+$lang['iconset_info_new_theme'] = 'Bu tema/temalar eklenmiştir:';
+$lang['iconset_nothing'] = 'orjinal ikonlar';
+$lang['iconset_seeall'] = 'Tümünü gör';
+$lang['iconset_send'] = 'Konfigürasyon değişikliklerini kaydet';
+$lang['iconset_update_ok'] = 'bu tema/temaların konfigürasyonu güncellenmiştir:';
+?>
