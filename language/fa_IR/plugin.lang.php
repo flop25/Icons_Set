fb554b00b210c810b055e9c38e4a94652a440f62
TranslationTeam <TranslationTeam@piwigo.org>
2012-07-07 14:08:38 +0000

    [icons_set] Update fa_IR, thanks to : Garcia
    git-svn-id: http://piwigo.org/svn/extensions/Icons_Set@16454 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fa_IR/plugin.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['iconset_All_Icons'] = 'همه ی دسته آیکون ها';
+$lang['iconset_All_Themes'] = 'همه ی قالب ها در زیر موجودند : یک دسته آیکون برای آنها انتخاب کنید';
+$lang['iconset_configpage'] = 'صفحه ی پیکربندی دسته آیکون ها';
+$lang['iconset_error_update'] = 'یک خطا هنگام بروز رسانی این قالب (ها) رخ داد (خواهشاً دوباره تلاش کرده یا مشکل خود را در piwigo.org/forum مطرح کنید) :';
+$lang['iconset_info_deleted_icon'] = 'این دسته آیکون (ها) پاک شده اند :';
+$lang['iconset_info_deleted_theme'] = 'این قالب (ها) پاک شده اند :';
+$lang['iconset_info_new_icon'] = 'این دسته آیکون (ها) افزوده شده اند :';
+$lang['iconset_info_new_theme'] = 'این قالب (ها) افزوده شده اند :';
+$lang['iconset_nothing'] = 'آیکون های اصلی';
+$lang['iconset_seeall'] = 'مشاهده ی همه';
+$lang['iconset_send'] = 'ذخیره کردن تغییرات انجام شده در پیکربندی ها';
+$lang['iconset_update_ok'] = 'پیکربندی این قالب (ها) بروز رسانی شد :';
+?>
