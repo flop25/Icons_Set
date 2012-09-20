01830d4a47c72cc1425d7f4fb2ebed99e0641ed0
TranslationTeam <TranslationTeam@piwigo.org>
2012-09-20 21:51:45 +0000

    [icons_set] Update da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/extensions/Icons_Set@18112 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['iconset_All_Icons'] = 'Alle ikonsættene';
+$lang['iconset_All_Themes'] = 'Alle temaerne er neden for: vælg et ikonsæt';
+$lang['iconset_configpage'] = 'Opsætning af ikonsæt';
+$lang['iconset_error_update'] = 'en fejl opstod under opdateringen af dette/disse tema(er) (prøv igen eller skriv et indlæg i piwigo.org/forum):';
+$lang['iconset_info_deleted_icon'] = 'Dette/disse ikonsæt er slettet:';
+$lang['iconset_info_deleted_theme'] = 'Dette/disse tema(er) er slettet:';
+$lang['iconset_info_new_icon'] = 'Dette/disse ikonsæt er tilføjet:';
+$lang['iconset_info_new_theme'] = 'Dette/disse tema(er) er tilføjet:';
+$lang['iconset_nothing'] = 'oprindelige ikoner';
+$lang['iconset_seeall'] = 'Se alle';
+$lang['iconset_send'] = 'Gem opsætningsændringerne';
+$lang['iconset_update_ok'] = 'opsætningen af dette/disse tema(er) er opdateret:';
+?>
