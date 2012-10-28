7b79e5a96556b1f8587542570c4a77a2f70ed44e
TranslationTeam <TranslationTeam@piwigo.org>
2012-10-28 18:24:40 +0000

    [icons_set] Add zh_CN, thanks to : foddyy
    git-svn-id: http://piwigo.org/svn/extensions/Icons_Set@18774 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['iconset_All_Icons'] = '所有图标集';
+$lang['iconset_All_Themes'] = '所有主题如下：选择一个图标集';
+$lang['iconset_configpage'] = '图标集配置页';
+$lang['iconset_error_update'] = '更新（这些）主题过程中发生错误（请重试或在piwigo.org/forum公布）：';
+$lang['iconset_info_deleted_icon'] = '（这些）图标集已被删除：';
+$lang['iconset_info_deleted_theme'] = '（这些）主题已被删除：';
+$lang['iconset_info_new_icon'] = '（这些）图标集已经添加：';
+$lang['iconset_info_new_theme'] = '（这些）主题已经添加：';
+$lang['iconset_nothing'] = '原始图标';
+$lang['iconset_seeall'] = '察看全部';
+$lang['iconset_send'] = '保存配置的变更';
+$lang['iconset_update_ok'] = '（这些）主题的配置已经更新：';
+?>
