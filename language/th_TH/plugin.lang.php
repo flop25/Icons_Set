446fb632d41e547cc0cbb071abe25fb13cb8842e
TranslationTeam <TranslationTeam@piwigo.org>
2012-12-07 05:40:21 +0000

    [icons_set] Add th_TH, thanks to : pradit
    git-svn-id: http://piwigo.org/svn/extensions/Icons_Set@19318 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/th_TH/plugin.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['iconset_All_Icons'] = 'ไอคอนทั้งหมดในชุด';
+$lang['iconset_All_Themes'] = 'ชุดรูปแบบทั้งหมดอยู่ด้านล่าง: เลือกชุดไอคอน';
+$lang['iconset_configpage'] = 'หน้าตั้งค่าชุดไอคอน';
+$lang['iconset_error_update'] = 'เกิดข้อผิดพลาดในระหว่างการปรับปรุงนี้ (โปรดลองอีกครั้งหรือโพสต์ขอความช่วยเหลือได้ที่ piwigo.org/forum) :';
+$lang['iconset_info_deleted_icon'] = 'ชุดไอคอน(นี้/เหล่านี้) ได้ถูกลบ :';
+$lang['iconset_info_deleted_theme'] = 'ธีม(นี้/เหล่านี้) ได้ถูกลบ :';
+$lang['iconset_info_new_icon'] = 'ชุดไอคอน(นี้/เหล่านี้) ได้ถูกเพิ่ม :';
+$lang['iconset_info_new_theme'] = 'ธีม(นี้/เหล่านี้) ได้ถูกเพิ่ม :';
+$lang['iconset_nothing'] = 'ไอคอนต้นฉบับ(ดั้งเดิม)';
+$lang['iconset_seeall'] = 'ดูทั้งหมด';
+$lang['iconset_send'] = 'บันทึกการปรับเปลี่ยนการตั้งค่า';
+$lang['iconset_update_ok'] = 'การตั้งค่า ธีมนี้ / ธีมเหล่านี้ ได้รับการอัพเดท:';
+?>
