cff96569158d8be90216362f1e064979e10197a3
TranslationTeam <TranslationTeam@piwigo.org>
2013-01-23 20:53:22 +0000

    [icons_set] Add pt_BR, thanks to : glaucio
    git-svn-id: http://piwigo.org/svn/extensions/Icons_Set@20346 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['iconset_update_ok'] = 'a configuração deste(s) tema(s) foi atualizada';
+$lang['iconset_send'] = 'salvar as modificações de configuração';
+$lang['iconset_nothing'] = 'ícones originais';
+$lang['iconset_seeall'] = 'Veja todos';
+$lang['iconset_info_new_theme'] = 'Este(s) tema(s) foi/foram adicionado:';
+$lang['iconset_info_new_icon'] = 'Este(s) conjunto(s) de ícones foi/foram adicionados:';
+$lang['iconset_info_deleted_theme'] = 'Este(s) tema(s) foi/foram apagados:';
+$lang['iconset_error_update'] = 'Ocorreu um erro durante a atualização deste(s) tema(s) (por favor tente de novo ou peça ajuda em piwigo.org/forum):';
+$lang['iconset_info_deleted_icon'] = 'Este(s) conjunto(s) de ícones foi/foram apagados:';
+$lang['iconset_configpage'] = 'Página de Configuração de Conjuntos de Ícones';
+$lang['iconset_All_Themes'] = 'Todos os temas estão abaixo : seleccione um conjunto de ícones';
+$lang['iconset_All_Icons'] = 'Todos os conjuntos de ícones';
+?>
