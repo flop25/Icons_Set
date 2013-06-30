6c23e961ced0ee5aa2045969cc5a238610d06cec
TranslationTeam <TranslationTeam@piwigo.org>
2013-06-30 10:38:45 +0000

    [icons_set] Add bg_BG, thanks to : turnokopa
    git-svn-id: http://piwigo.org/svn/extensions/Icons_Set@23710 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/bg_BG/plugin.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['iconset_update_ok'] = 'настройките за тази(тези) тема(и) бяха обновени:';
+$lang['iconset_send'] = 'Запишете промяната на конфигурацията';
+$lang['iconset_seeall'] = 'Виж всички';
+$lang['iconset_nothing'] = 'оригинална икона';
+$lang['iconset_info_deleted_theme'] = 'Тази(тези) тема(и) е(са) изтрит(и):';
+$lang['iconset_info_new_icon'] = 'Този(тези) набор от икони е(са) добавен(и):';
+$lang['iconset_info_new_theme'] = 'Тази(тези) тема(и) е(са) добавена(и):';
+$lang['iconset_info_deleted_icon'] = 'Тази(тези) икона(и) е(са) изтрит(и):';
+$lang['iconset_error_update'] = 'възникна грешка по време на актуализацията на тази(тези) тема(и) (моля опитайте отново или публикувате на www.piwigo.org/forum):';
+$lang['iconset_configpage'] = 'Страница за конфигуриране на групи икони';
+$lang['iconset_All_Icons'] = 'Всички избрани икони';
+$lang['iconset_All_Themes'] = 'За всички изредени теми : избери група икони';
+?>
