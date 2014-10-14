939d6bbb7faad1690ced60fb9636eae862254f19
TranslationTeam <TranslationTeam@piwigo.org>
2014-10-14 19:37:31 +0000

    [icons_set] Update nb_NO, thanks to : paulen
    git-svn-id: http://piwigo.org/svn/extensions/Icons_Set@30063 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nb_NO/plugin.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['iconset_All_Icons'] = 'Alle ikon settene';
+$lang['iconset_All_Themes'] = 'Alle temaene er under: Velg et ikon sett';
+$lang['iconset_configpage'] = 'Ikon setts innstillings side';
+$lang['iconset_error_update'] = 'Innstillinsside for ikon sett';
+$lang['iconset_info_deleted_icon'] = 'Dette/Disse ikon sett(ene) har blitt slettet:';
+$lang['iconset_info_deleted_theme'] = 'Dette/Disse tema har blitt slettet:';
+$lang['iconset_info_new_icon'] = 'Dette/Disse ikon sett(ene) har blitt lagt til:';
+$lang['iconset_info_new_theme'] = 'Dette/Disse tema har blitt lagt til:';
+$lang['iconset_nothing'] = 'originale ikons';
+$lang['iconset_seeall'] = 'Se alle';
+$lang['iconset_send'] = 'Lagre endringen av innstillingen';
+$lang['iconset_update_ok'] = 'instillingen til dette/disse tema har blitt oppdatert:';
