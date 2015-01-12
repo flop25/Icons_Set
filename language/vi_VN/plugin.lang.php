abdc71010a88159737fe9476af3bcf88d956a297
TranslationTeam <TranslationTeam@piwigo.org>
2015-01-12 05:16:37 +0000

    Update vi_VN, thanks to dummy
    git-svn-id: http://piwigo.org/svn/extensions/Icons_Set@30884 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/vi_VN/plugin.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['iconset_All_Icons'] = 'Tất cả bộ biểu tượng';
+$lang['iconset_All_Themes'] = 'Tất cả giao diện bên dưới : chọn một bộ biểu tượng';
+$lang['iconset_configpage'] = 'Trang cấu hình bộ biểu tượng';
+$lang['iconset_error_update'] = 'đã xảy ra lỗi trong quá trình cập nhật giao diện này (vui lòng thử lại hoặc gửi vào piwigo.org/forum) :';
+$lang['iconset_info_deleted_icon'] = 'Biểu tượng này đã được xóa :';
+$lang['iconset_info_deleted_theme'] = 'Giao diện này đã được xóa :';
+$lang['iconset_info_new_icon'] = 'Biểu tượng này đã được thêm :';
+$lang['iconset_info_new_theme'] = 'Giao diện này đã được thêm :';
+$lang['iconset_nothing'] = 'các biểu tượng gốc';
+$lang['iconset_seeall'] = 'Xem tất cả';
+$lang['iconset_send'] = 'Lưu các chỉnh sửa cấu hình';
+$lang['iconset_update_ok'] = 'cấu hình của giao diện này đã được cập nhật :';
